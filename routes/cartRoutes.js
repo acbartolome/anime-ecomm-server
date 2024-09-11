91e6914860d65c7e8df9df59e6ffd5601f0ad3fd
adryanbartolome <adryanbartolome8123@gmail.com>
2024-09-11 16:19:56 -0700

    Corrected .env and removed comments

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,29 +1,3 @@
-// //For Users
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   viewCartItem,
-//   addToCart,
-//   removeItem,
-// } = require("../controllers/cartController");
-
-// // view cart based on ID
-// router.get("/", viewCartItem);
-
-// //add to cart
-// router.post("/", addToCart);
-
-// //edit the items in your cart using Patch
-// //identify the amount of items in the cart
-// router.patch("/:id", (req, res) => {
-//   console.log("edit item in cart");
-// });
-
-// //remove an item from your cart using Patch
-// router.delete("/", removeItem);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const {
